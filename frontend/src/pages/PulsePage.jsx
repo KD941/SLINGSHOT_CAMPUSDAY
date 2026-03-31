d779c62811d5455ef494a50a5f4e3d214813738b
KD941 <karmdiprathva9@gmail.com>
2026-03-31 15:59:57 +0530

    docker

---
+++ frontend/src/pages/PulsePage.jsx
@@ -0,0 +1,3 @@
+// Pulse page: re-exports the exercise/streak content from FastingRegimens
+// The Stitch design has "Pulse Exercises" as its own dedicated tab screen.
+export { default } from './FastingRegimens';
